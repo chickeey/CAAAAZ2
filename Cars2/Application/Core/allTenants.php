b20bc07518d50bc720da7b5be1c8566ea96ced89
Simon Gergő <55582437+chickeey@users.noreply.github.com>
2020-03-05 23:48:25 +0100

    Update allTenants.php

--- Cars2/Application/Core/allTenants.php
+++ Cars2/Application/Core/allTenants.php
@@ -1,1 +1,11 @@
 <?php
+
+$producer = @$_POST['Tentants'] ? $_POST['Tentants'] : null;
+
+$config = getConfig($configPath);
+
+$pdo = getConection( $config["database"] );
+
+$producers = getTentants( $pdo );
+
+$cars = getCars( $pdo, $producer );
